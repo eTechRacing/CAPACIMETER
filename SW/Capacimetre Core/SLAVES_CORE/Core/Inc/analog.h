a6cce7f223e30daf2189ba1695b19dab5a37552b
Becato7 <bernatcarnota@gmail.com>
2024-10-16 15:12:49 +0200

    Capacimetre core

---
+++ SW/Capacimetre Core/SLAVES_CORE/Core/Inc/analog.h
@@ -0,0 +1,13 @@
+/*
+ * analog.h
+ *
+ *  Created on: Oct 16, 2024
+ *      Author: e-Tech Racing
+ */
+
+#ifndef INC_ANALOG_H_
+#define INC_ANALOG_H_
+
+
+
+#endif /* INC_ANALOG_H_ */
---
+++ SW/Capacimetre Core/SLAVES_CORE/Core/Src/analog.c
@@ -0,0 +1,8 @@
+/*
+ * analog.c
+ *
+ *  Created on: Oct 16, 2024
+ *      Author: e-Tech Racing
+ */
+
+
